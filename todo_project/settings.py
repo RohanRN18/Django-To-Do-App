27a1063242f92efbb3487356085893c7610e8e65
Rohan Nannware <127673577+RohanRN18@users.noreply.github.com>
2025-10-08 15:36:24 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i&1u5fs4_(np^9y#ys&8e$50ag1@awn+f(7@bzclu4t*d20%#^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
